279cab1739f39710da87b1c200a5661bfaf6176a
gcoradello <guilhermecoradellosilveira@gmail.com>
2026-04-10 00:18:22 -0300

    Ajustando backend

---
+++ backend/src/test/java/com/extensao/mvp/MvpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.extensao.mvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
